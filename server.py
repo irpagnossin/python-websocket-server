7bf09d731777cacb985e1478b3acff8014c707f8
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2017-02-02 00:08:45 -0200

    work in progress

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from websocket_server import WebsocketServer
+import json
+
+users = []
+
 
 # Called for every client connecting (after handshake)
 def new_client(client, server):
@@ -13,9 +17,54 @@ def client_left(client, server):
 
 # Called when a client sends a message
 def message_received(client, server, message):
+	msg = json.loads(message)
+
+	incoming_action = str(msg['action'])
+
+	if incoming_action == 'USER_SAYS':
+		server.send_message_to_all(message)
+
+	elif incoming_action == "SIGN_IN":
+
+		new_user = {
+			'username': str(msg['user']),
+			'room': str(msg['room'])
+		}
+		users.append(new_user)
+
+		outgoing_socket_message = {
+			'action': "USER_IN",
+			'message': str(msg['message']),
+			'room': str(msg['room']),
+			'user': str(msg['user'])
+		}
+		print "avisando outros: %s" % json.dumps(outgoing_socket_message)
+		server.send_message_to_all(json.dumps(outgoing_socket_message))
+
+	elif incoming_action == "SIGN_OUT":
+		print "<<sign-out>>"
+
+	elif incoming_action == "REQUEST_USERS":
+		_users = filter(lambda user: user['room'] == msg['room'], users)
+		_usernames = map(lambda user: user['username'], _users)
+		outgoing_socket_message = {
+			'action': "ALL_USERS",
+			'message': ','.join(_usernames),
+			'room': "",
+			'user': ""
+		}
+		print "Informando usuarios existentes: %s" % json.dumps(outgoing_socket_message)
+		server.send_message(client, json.dumps(outgoing_socket_message))
+		pass
+
+	else:
+		print "I don't know what you mean!"
+
 	if len(message) > 200:
 		message = message[:200]+'..'
 	print("Client(%d) said: %s" % (client['id'], message))
+
+
 
 
 PORT=9001
